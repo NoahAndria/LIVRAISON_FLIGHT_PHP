d8035170a01a63058969c8133fdb5f9d1ad62644
Uchi Fiti <fitiuchi@gmail.com>
2025-12-18 13:57:24 +0300

    Revert "new version replacing the old one"
    This reverts commit 00878fc7f1e820504a51e772d92315db85f93312.

--- livraison/src/app/controllers/LivraisonController.php
+++ livraison/src/app/controllers/LivraisonController.php
@@ -3,15 +3,12 @@ namespace app\controllers;
 use flight\Engine;
 use Flight;
 use app\models\Produit;
-use app\models\Livraison;
-use app\models\Entrepot;
-use app\models\Chauffeur;
 class LivraisonController {
     protected Engine $app;
     public function __construct($app) {
         $this->app = $app;
     }
-    public function passVariables() {
+    public function getProducts() {
         $nonce = base64_encode(random_bytes(16));
     
         // Set CSP header
@@ -19,31 +16,13 @@ class LivraisonController {
             'Content-Security-Policy',
             "script-src 'self' 'nonce-{$nonce}' 'strict-dynamic';"
         );
+        
         // Pass nonce to template
         $prod = new Produit();
 		$this->app->render('ajout_livraison', [
             'products' => $prod->getProducts($this->app->db()),
-            'entrepots' => $this->getEntrepots(),
-            'chauffeurs' => $this->getChauffeurs(),
             'nonce' => $nonce
         ]);
-    }
-    public function getEntrepots() {
-        $e = new Entrepot();
-        return $e->findAll($this->app->db());
-    }
-    public function getChauffeurs() {
-        $c = new Chauffeur();
-        return $c->findAll($this->app->db());
-    }
-    public function handleLivraison() {
-		$req = Flight::request();
-        $l = new Livraison();
-        if($l->livrer($this->app->db(), $req->data) == 0) {
-            echo json_encode(["status" => "success"]);
-        } else {
-            echo json_encode(["status" => "failure"]);
-        }
     }
 }
 ?>
